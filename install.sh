fd2903b8e73ba30ced43dbc37d78118214b52d6e
ZacamaX <50320281+Zacama@users.noreply.github.com>
2023-09-28 09:56:39 +0800

    add test files

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+npm install -g @asyncapi/cli@0.56.3
+PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm install -g @asyncapi/html-template@0.28.4
---
+++ same_output_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+files=(1 2 3 4)
+
+for file in "${files[@]}"
+do
+    asyncapi generate fromTemplate asyncapis/$file.yaml @asyncapi/html-template --param singleFile=true outFilename=$file.html --output ./generate --force-write &
+done
+wait
