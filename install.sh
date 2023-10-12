362b1d7574dbee685503b5eccccfbce2df1098e9
ZacamaX <50320281+Zacama@users.noreply.github.com>
2023-10-12 05:45:50 +0000

    [chore] update version @asyncapi/html-template@1.0.0

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 npm install -g @asyncapi/cli@0.56.3
-PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm install -g @asyncapi/html-template@0.28.4
+PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm install -g @asyncapi/html-template@1.0.0
